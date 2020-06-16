555b86263acbddde964a4217fd5f7b5709e3cc39
的場郁美 <>
2020-06-16 13:25:12 +0900

    html（パーフェクトピクセル実施前）

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
   <main>
     <div>
       <!-- 左上 -->
-      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="main" class="top-image">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="top-left" class="top-left">
     </div>
 
     <?php if (get_header_image()): ?>
-      <img src="<?php header_image(); ?>" alt="" class="top-image">
+      <img src="<?php header_image(); ?>" alt="main" class="top-image">
     <?php else: ?>
       <img src="<?php echo get_template_directory_uri(); ?>/img/visual_1_pc.png" alt="main" class="top-image">
     <?php endif;?>
 
-    <div>
+    <div class="bottom-right">
       <!-- 右下 -->
-      <img src="<?php echo get_template_directory_uri(); ?>/img/visual_text_pc.png" alt="main" class="top-image">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/visual_text_pc.png" alt="bottom-right">
     </div>
 
     <!-- 文言 -->
@@ -28,7 +28,7 @@
       <ul class="articles">
         <?php while(have_posts()): the_post(); ?>
           <li class="article">
-            <a href="<?php the_permalink(); ?>">
+            <a href="<?php the_permalink(); ?>"  class="article-img">
               <?php if (has_post_thumbnail()): ?>
                 <?php the_post_thumbnail('thumbnail'); ?>
               <?php else: ?>
@@ -36,8 +36,8 @@
               <?php endif; ?>
             </a>
 
-            <p><?php the_time("Y/m/d/"); ?></p>
-            <p class="article-content"><?php the_excerpt(); ?></p>
+            <p class="article-date"><?php the_time("Y/m/d/"); ?></p>
+            <p class="article-title"><?php the_title(); ?></p>
             <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
           </li>
         <?php endwhile; ?>
