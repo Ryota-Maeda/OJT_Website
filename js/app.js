cd70769b62f43580d6001e6aad8cf84901a50cd8
的場郁美 <>
2020-06-18 16:06:11 +0900

    スライド追加

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(function () {
+
+  $('.nav_toggle').on('click', function () {
+    $('.nav_toggle, .nav').toggleClass('show');
+
+  });
+});
+
+$(function(){
+  $(".fade-pc img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".fade-pc img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-pc");
+  },3500);
+});
+$(function(){
+  $(".fade-sp img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".fade-sp img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-sp");
+  },3500);
+});
