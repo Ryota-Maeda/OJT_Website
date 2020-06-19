6ee2c502b75ff1418696b3d5914ac3c23cf5a3e0
的場郁美 <>
2020-06-19 15:16:40 +0900

    記事一覧追加

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ $(function(){
   $(".fade-pc img:not(:first-child)").hide();
   setInterval(function() {
     $(".fade-pc img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-pc");
-  },3500);
+  },3000);
 });
 $(function(){
   $(".fade-sp img:not(:first-child)").hide();
   setInterval(function() {
     $(".fade-sp img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-sp");
-  },3500);
+  },3000);
 });
